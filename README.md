cd42223b03da5de3407d3cd47dd8ca78888a8224
Daniel Mascarenhas <daniel.mascr@gmail.com>
2018-05-24 23:15:34 -0300

    Add arquivos Composicao

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Repositório de apoio aos meus estudos relacionados a linguagem PHP.
+
+Obs. Todos os exemplos presentes neste repositório fazem parte do estudo do livro 
+
+PHP - Programando com Orientação a Objetos de Pablo Dall'Oglio.
--- fundamentos_oo/classes/Produto.php
+++ fundamentos_oo/classes/Produto.php
@@ -5,11 +5,20 @@
         private $estoque;
         private $preco;
         private $fabricante;
+        private $caracteristicas;
 
         public function __construct( $descricao, $estoque, $preco ){
             $this->descricao = $descricao;
             $this->estoque = $estoque;
             $this->preco = $preco;
+        }
+        
+        public function setFabricante( Fabricante $f){
+            $this->fabricante = $f;
+        }
+        
+        public function addCaracteristica( $nome, $valor ){
+            $this->caracteristicas[] = new Caracteristica( $nome, $valor );
         }
 
         public function getDescricao(){
@@ -24,12 +33,12 @@
             return $this->preco;
         }
 
-        public function setFabricante( Fabricante $f){
-            $this->fabricante = $f;
-        }
-
         public function getFabricante(){
             return $this->fabricante;
+        }
+
+        public function getCaracteristicas(){
+            return $this->caracteristicas;
         }
     }
 
