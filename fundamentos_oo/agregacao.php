2c683d0a859c900f5fa1c91c204ca607272d4504
Daniel Mascarenhas <daniel.mascr@gmail.com>
2018-06-01 03:01:23 -0300

    Add arquivos agregacao

---
+++ fundamentos_oo/agregacao.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'classes/Cesta.php';
+require_once 'classes/Produto.php';
+
+// criacao da cesta
+$c1 = new Cesta;
+
+// agregacao dos produtos
+$c1->addItem( $p1 = new Produto('Chocolate', 10, 5) );
+$c1->addItem( $p2 = new Produto('Café', 100, 7) );
+$c1->addItem( $p3 = new Produto('Mostarda', 50, 3) );
+
+foreach ($c1->getItens() as $item) {
+    print 'Item: '. $item->getDescricao() ."<br>\n";
+}
+
+?>
