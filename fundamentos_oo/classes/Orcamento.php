d7d940c341399b055ec60b0e1a3ad676f5f5ec30
Daniel Mascarenhas <daniel.mascr@gmail.com>
2018-06-14 23:35:02 -0300

    Add arquivos interface

---
+++ fundamentos_oo/classes/Orcamento.php
@@ -0,0 +1,19 @@
+<?php
+
+class Orcamento {
+    private $itens;
+
+    public function adiciona(OrcavelInterface $produto, $quantidade) {
+        $this->itens[] = array($quantidade, $produto);
+    }
+
+    public function calculaTotal() {
+        $total = 0;
+        foreach ($this->itens as $item) {
+            $total += ($item[0] * $item[1]->getPreco());
+        }
+        return $total;
+    }
+}
+
+?>
--- fundamentos_oo/classes/Produto.php
+++ fundamentos_oo/classes/Produto.php
@@ -1,45 +1,45 @@
 <?php
 
-    class Produto{
-        private $descricao;
-        private $estoque;
-        private $preco;
-        private $fabricante;
-        private $caracteristicas;
+class Produto implements OrcavelInterface {
+    private $descricao;
+    private $estoque;
+    private $preco;
+    private $fabricante;
+    private $caracteristicas;
 
-        public function __construct( $descricao, $estoque, $preco ){
-            $this->descricao = $descricao;
-            $this->estoque = $estoque;
-            $this->preco = $preco;
-        }
-        
-        public function setFabricante( Fabricante $f){
-            $this->fabricante = $f;
-        }
-        
-        public function addCaracteristica( $nome, $valor ){
-            $this->caracteristicas[] = new Caracteristica( $nome, $valor );
-        }
-
-        public function getDescricao(){
-            return $this->descricao;
-        }
-
-        public function getEstoque(){
-            return $this->estoque;
-        }
-
-        public function getPreco(){
-            return $this->preco;
-        }
-
-        public function getFabricante(){
-            return $this->fabricante;
-        }
-
-        public function getCaracteristicas(){
-            return $this->caracteristicas;
-        }
+    public function __construct( $descricao, $estoque, $preco ){
+        $this->descricao = $descricao;
+        $this->estoque = $estoque;
+        $this->preco = $preco;
     }
+    
+    public function setFabricante( Fabricante $f){
+        $this->fabricante = $f;
+    }
+    
+    public function addCaracteristica( $nome, $valor ){
+        $this->caracteristicas[] = new Caracteristica( $nome, $valor );
+    }
+
+    public function getDescricao(){
+        return $this->descricao;
+    }
+
+    public function getEstoque(){
+        return $this->estoque;
+    }
+
+    public function getPreco(){
+        return $this->preco;
+    }
+
+    public function getFabricante(){
+        return $this->fabricante;
+    }
+
+    public function getCaracteristicas(){
+        return $this->caracteristicas;
+    }
+}
 
 ?>
